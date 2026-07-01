1ba69e52b11483194fc9eec3623142a4af1708c5
zzobaer09 <zzobaer09@gmail.com>
2026-07-01 23:45:42 +0600

    problem3

---
+++ problem03.cpp
@@ -0,0 +1,17 @@
+/*
+    https://vjudge.net/contest/826394#problem/C
+*/
+
+#include<iostream>
+#include<string>
+#include<sstream>
+
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    string s = "kjka";
+
+    stringstream ss(s);
+
+    return 0;
+}
