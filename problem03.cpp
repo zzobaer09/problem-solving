0c3f74f002ef1751c1faafdb6a9c8dc2e524fe66
zzobaer09 <zzobaer09@gmail.com>
2026-07-02 00:29:31 +0600

    problem 3

--- problem03.cpp
+++ problem03.cpp
@@ -5,13 +5,25 @@
 #include<iostream>
 #include<string>
 #include<sstream>
+#include<vector>
 
 using namespace std;
+
 int main(int argc, char const *argv[])
 {
-    string s = "kjka";
-
+    string s;
+    cin>>s;
     stringstream ss(s);
 
+    long a,b,c;
+    char ch;
+    vector<int> arr;    
+    while (!ss.eof())
+    {
+        int temp;
+        ss>>temp>>ch;
+        arr.push_back(temp);
+    }
+    for (auto &&i : arr) cout<<i<<endl;    
     return 0;
 }
