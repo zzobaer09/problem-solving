a73f3e4a8ebd599c73ed411c44c12cc043245bdd
zzobaer09 <zzobaer09@gmail.com>
2026-07-08 14:24:12 +0600

    flag variable

--- problem09.cpp
+++ problem09.cpp
@@ -23,23 +23,29 @@ int main(int argc, char const *argv[])
 
     sort(vt.rbegin(),vt.rend());
     int counter = 0;
+
+    bool flag = true;
+
     for (int i = 0; i < n; i++)
     {
         if(vt[i]==-1) continue;
         int temp_sum = vt[i];
         
-        for (int j = i+1; j < n; j++)
+        for (int j = i+1; j<n && flag; j++)
         {
             if(vt[j]==-1) continue;
+
             if(temp_sum+vt[j]<=x){
                 temp_sum += vt[j];
                 vt[j] = -1;
+                flag = false;
             }             
         }
 
         counter++;
         temp_sum = 0;
         vt[i] = -1;
+        flag = true;
     }
     cout<<counter<<endl;
     return 0;
