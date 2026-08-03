64f42649ad3fec29c2c511c43839fe6106f29452
zzobaer09 <zzobaer09@gmail.com>
2026-08-04 00:42:27 +0600

    v1

---
+++ 100/problem34.cpp
@@ -0,0 +1,38 @@
+/*
+    https://codeforces.com/problemset/problem/4/C
+*/
+
+#include<bits/stdc++.h>
+
+typedef long long ll;
+typedef long double ld;
+
+using namespace std;
+
+
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n; cin>>n;
+
+    map<string,int> usrname;
+
+
+    while (n--)
+    {
+        string usr; cin>>usr;
+        
+        if(usrname[usr]>=1){
+
+            string newusr = usr+ to_string(usrname[usr]);
+            cout<<newusr<<endl;
+            usrname[usr]++;
+            usrname[newusr]++;
+        }else{
+            usrname[usr]++;
+            cout<<"OK"<<endl;
+        }
+    }
+    
+}
