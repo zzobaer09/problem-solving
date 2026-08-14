4004a01b2eb7d6388fa3fd560abd70f3c45fb8b2
zzobaer09 <zzobaer09@gmail.com>
2026-08-14 08:24:24 +0600

    template

--- mytemplate.cpp
+++ mytemplate.cpp
@@ -1,16 +1,16 @@
 /*
     PROBLEM LINK
 */
-
+ 
 #include<bits/stdc++.h>
-
+ 
 typedef long long ll;
 typedef long double ld;
-
+ 
 using namespace std;
-
-int main(int argc, char const *argv[])
-{
-    /* code */
-    return 0;
+ 
+ 
+int main(int argc, char const *argv[]){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 }
