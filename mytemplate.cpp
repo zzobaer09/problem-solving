8ea7b5112f12cf292c39e1c9e95724d47582429a
zzobaer09 <zzobaer09@gmail.com>
2026-09-19 23:57:54 +0600

    https://cses.fi/problemset/task/1094

--- mytemplate.cpp
+++ mytemplate.cpp
@@ -1,5 +1,5 @@
 /*
-    PROBLEM LINK
+    https://cses.fi/problemset/task/1094
 */
  
 #include<bits/stdc++.h>
@@ -13,4 +13,18 @@ using namespace std;
 int main(int argc, char const *argv[]){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    
+    int n; cin>>n;
+
+    vector<ll> a(n);
+    
+    for(ll &i:a) cin>>i;
+    ll count=0;
+    for(int i=1;i<n;i++){
+        if(a[i]<a[i-1]) {
+            count+=a[i-1]-a[i];
+            a[i]=a[i-1];
+        }
+    }
+    cout<<count<<endl;
 }
